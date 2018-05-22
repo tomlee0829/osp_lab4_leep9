e92cab241c5a0b90aa695baedaeac3e19d05c0f2
Lee <leep9@debian-9.3.0-template>
2018-05-22 03:24:13 -0700

    Initial Commit: declare headers and function for lab4

---
+++ fileSearch.c
@@ -0,0 +1,22 @@
+//Description: Lab program of recursive file search in C,
+//prints all files and deirectories whose names contain 
+//the search term and also print the time it took
+//to do the search.
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <dirent.h>           
+#include <sys/stat.h>           
+#include <string.h> 
+#include <time.h>
+
+void fileSearch(char *searchTerm)
+{
+
+}
+
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
